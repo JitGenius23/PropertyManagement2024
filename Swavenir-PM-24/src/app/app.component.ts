83ffbbb5c6c4efd29a22ad4a72136f42e6daefd1
subhrajit sahoo <subhrajitsahoos85@gmail.com>
2024-10-09 11:24:05 -0700

    website layout completed

--- Swavenir-PM-24/src/app/app.component.ts
+++ Swavenir-PM-24/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { CareerComponent } from './features/career/career.component';
+import { ServicesComponent } from './features/services/services.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { PricingComponent } from './features/pricing/pricing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    CareerComponent,
+    ServicesComponent,
+    ContactComponent,
+    PricingComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Swavenir-PM-24';
